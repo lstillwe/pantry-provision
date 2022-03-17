c2cf7f6303e29dfb1214486a56285ebc3f431739
Lisa Stillwell <lisastillwell@bellsouth.net>
2022-03-17 16:32:48 -0400

    added server typedefs and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,46 @@
 import gql from 'graphql-tag';
+
+// create our typeDefs
+const typeDefs = gql`
+type User {
+    _id: ID
+    email: String
+    items: [Item]
+}
+type Item {
+    itemId: ID
+    name: String
+    price: Float
+    quantity: Int
+    threshold: Int
+    storage: String
+    category: String
+}
+type Auth {
+    token: ID!
+    user: User
+}
+type Query {
+    me: User
+}
+input ItemInput {
+    itemId: ID
+    name: String
+    price: Float
+    quantity: Int
+    threshold: Int
+    storage: String
+    category: String
+}
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addItem(itemData: ItemInput!): User
+    updateItem(itemData: ItemInput!): User
+    deleteItem(itemId: ID!): User
+    updateInventory(itemId: ID!, quantity: Int!): User
+}
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
